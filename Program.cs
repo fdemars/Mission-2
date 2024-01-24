f7808c3e3bdabba4207d12cc0c97eceba46dd0b3
fieldingdemars <fieldingdemars@FIELDINGDEMEF01>
2024-01-24 12:23:29 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using Mission_2;
+
+internal class DiceSimulator
+{
+    
+
+    private static void Main(string[] args)
+    {
+        // create a RollDice instance
+        RollDice rd = new RollDice();
+
+        int requested_num_roll = 0;
+
+        // prompt the user to enter the desired number of rolls
+        Console.WriteLine("Welcome to the dice throwing simulator!" + "\n");
+        Console.WriteLine("How many dice rolls would you like to simulate?" + "\n");
+        requested_num_roll = int.Parse(Console.ReadLine());
+        
+        // plug the desired number of rolls into the RollDice class
+        rd.PrintResult(requested_num_roll);
+
+        
+        
+    }
+}
---
+++ RollDice.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+// break down the logic
+// need to use an array
+
+// allow user to choose the number of rolls
+
+// *second class
+// receive the number of rolls
+
+// for that number of rolls
+// simulate the roll of die1
+// simulate the roll of die2
+// get the total
+// for that total, add 1 to the results
+
+//return the results to the first class
+
+//*first class
+//for each possible number
+// print the number
+// calculate what percentage that number was rolled
+
+// print astericks for each percentage
+
+
+
+namespace Mission_2
+{
+    internal class RollDice
+    {
+        public RollDice()
+        {
+
+        }
+
+        public void PrintResult(int roll_req)
+        {
+
+            // call the random number generator class
+            Random r = new Random();
+
+            // this is my code to run through until the number of rolls is reached
+            int die1 = 0;
+            int die2 = 0;
+
+            int[] results = new int[13];
+
+            // simulate the roll for the number of times the user specified. save the number rolled in an array.
+            for (int count = 0; count < roll_req; count++)
+            {
+                die1 = r.Next(1, 7);
+                die2 = r.Next(1, 7);
+
+                results[die1 + die2]++;
+            }
+
+            // Write the printed code per assignment criteria
+            Console.WriteLine("\n" + "DICE ROLLING SIMULATION RESULTS" + "\n" +
+                "Each '*' represents 1% of the total number of rolls." + "\n" +
+                "Total number of rolls = " + roll_req + "." + "\n");
+
+            // Print results
+            for (int i = 2; i < 13; i++)
+            {
+
+                // calculate what percentage that number was rolled. print astericks for each percentage
+                int percentage = results[i] * 100 / roll_req;
+        
+                Console.WriteLine(i + ": " + new string('*', percentage));
+            }
+
+            Console.WriteLine("\n" + "Thank you for using the dice throwing simulator. Goodbye!");
+        }
+    }
+}
